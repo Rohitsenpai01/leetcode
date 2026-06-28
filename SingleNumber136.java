6821760ac54665a06b08b3f3e66c44007930e75a
Rohitsenpai01 <rtakale103@gmail.com>
2026-06-28 19:20:10 +0530

    problem number 136

---
+++ SingleNumber136.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class SingleNumber136 {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
